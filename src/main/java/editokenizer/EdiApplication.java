9b3b88d5b7a9fb40dea60f8790f36bfd44865601
Kavindra Kulathilake <kkulathilake@gmail.com>
2019-03-19 00:06:02 +0100

    Initial project commit

---
+++ src/main/java/editokenizer/EdiApplication.java
@@ -0,0 +1,12 @@
+package editokenizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdiApplication.class, args);
+    }
+}
