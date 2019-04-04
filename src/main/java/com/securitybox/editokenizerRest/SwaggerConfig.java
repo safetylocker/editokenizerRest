1d7da19e0cb51cd95073ca92b8c4a96639aa92fc
Kavindra Kulathilake <kkulathilake@gmail.com>
2019-04-04 12:01:06 +0200

    path selectors added to config

--- src/main/java/com/securitybox/editokenizerRest/SwaggerConfig.java
+++ src/main/java/com/securitybox/editokenizerRest/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -29,7 +30,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 //.paths(regex("/tokernizer.*"))
                 //.build();
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.securitybox.editokenizerRest")).build()
+                .apis(RequestHandlerSelectors.basePackage("com.securitybox.editokenizerRest")).paths(PathSelectors.any()).build()
                 .apiInfo(apiInfo());
 
     }
